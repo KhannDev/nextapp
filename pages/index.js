2e9c2d0c870a4e2a6f1c87ac25b398f8fc80d333
KhannDev <muaaz@onpar.in>
2023-01-17 23:55:59 +0530

    test15

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Lets Get started  by editing&nbsp;
+            Lets us please  Get started  by editing&nbsp;
             <code className={styles.code}>pages/index.js</code>
           </p>
           <div>
